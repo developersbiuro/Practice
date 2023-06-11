7896b5119aae04c515b9d2e6d0276ae18065cf79
developersbiuro <developersbiuro@gmail.com>
2023-06-11 14:15:10 +0500

    2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var input = [
     {userName : 'ali', userEmail: 'ali@gmail.com',   dob : '15-8-2002' },
     {userName : 'ahmad', userEmail: 'ahmad@gmail.com',   dob : '12-10-2004' },
-    {userName : 'ali', userEmail: 'ali@gmail.com',   dob : '25-5-1998' }  
+    {userName : 'akbar', userEmail: 'akbar@gmail.com',   dob : '25-5-1998' }  
 ]
 
