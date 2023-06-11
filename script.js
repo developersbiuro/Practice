767915194ba4e432ff33611fafe36e36cf7acdab
developersbiuro <developersbiuro@gmail.com>
2023-06-11 14:09:58 +0500

    1

---
+++ script.js
@@ -0,0 +1,6 @@
+var input = [
+    {userName : 'ali', userEmail: 'ali@gmail.com',   dob : '15-8-2002' },
+    {userName : 'ahmad', userEmail: 'ahmad@gmail.com',   dob : '12-10-2004' },
+    {userName : 'ali', userEmail: 'ali@gmail.com',   dob : '25-5-1998' }  
+]
+
