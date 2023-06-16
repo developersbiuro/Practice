42360bbffe1ca30281e6a452786bbf80ac145bee
developersbiuro <developersbiuro@gmail.com>
2023-06-16 14:27:35 +0500

    js

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-var input = [
-    {userName : 'ali', userEmail: 'ali@gmail.com',   dob : '15-8-2002' },
-    {userName : 'ahmad', userEmail: 'ahmad@gmail.com',   dob : '12-10-2004' },
-    {userName : 'akbar', userEmail: 'akbar@gmail.com',   dob : '25-5-1998' }  
-]
+// var input = [
+//     {userName : 'ali', userEmail: 'ali@gmail.com',   dob : '15-8-2002' },
+//     {userName : 'ahmad', userEmail: 'ahmad@gmail.com',   dob : '12-10-2004' },
+//     {userName : 'akbar', userEmail: 'akbar@gmail.com',   dob : '25-5-1998' }  
+// ]
+// console.log(input);
+// var Dob = new Date(input.dob).getFullYear();
+// var now  = new Date;
+// var calc = now - Dob
+// console.log(Dob);
+// console.log(calc);
+// console.log(now);
 
-var Dob = new getFullYear(input.dob);
-var now  = new Date(year);
-var calc = now - Dob
-console.log(Dob);
-console.log(calc);
-console.log(now);
+
+
+var userName = document.getElementById("nam").value;
+var userMail = document.getElementById("mail").value;
+var userCncd = document.getElementById("cncd").value;
+var userBio = document.getElementById("Bio").value;
+var userState = document.getElementById("stat").value;
+var userCnic = document.getElementById("cn").value;
+
+function LoginPage() {
+    if (userName.includes(" ")) {
+        
+    }
+    
+}
